4a94a4af71a1364f5ea51ce80d6d74da753710dc
rafiramadhan <rafirama1020@gmail.com>
2025-11-21 13:29:43 +0700

    Create guide1.go

---
+++ Rafi Ramadhan/guide1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var usia int
+	var punyaKK bool
+
+	fmt.Scan(&usia)
+	fmt.Scan(&punyaKK)
+
+	if usia >= 17 && punyaKK {
+			fmt.Println("bisa buat KTP")
+		} else {
+			fmt.Println("belum bisa membuat ktp")
+		}
+}
